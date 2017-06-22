b449b5ff048492e8c4f31a8ccb21dfe8a8a0b0d2
kubawieczorek <jwieczorek94@wp.pl>
2017-06-22 21:48:22 +0200

    Rozbudowa algorytmu
    Rozbudowa algorytmu - zarys + obliczanie objętosci

--- src/com/fuelalg/FuelAlg.java
+++ src/com/fuelalg/FuelAlg.java
@@ -8,26 +8,55 @@ import java.util.ArrayList;
 public class FuelAlg {
 
 
+    public ArrayList<Double> outputFluidVolumes;
+    public ArrayList<Double> outputFluidTemps;
+
     private ArrayList<Double> ambientTempList;
     private double startFluidTemp;
     private double startFluidVolume;
     private double newFluidVolume;
     private double newFluidTemp;
     private double newAmbientTemp;
+    private double oldAmbientTemp;
+    private double oldFluidTemp;
 
-    public FuelAlg(double _startFluidTemp, double _startFluidVolume,ArrayList<Double> _ambientTempList ){
+    private double thermalExpansionCoefficient;
+
+    public FuelAlg(double _startFluidTemp, double _startFluidVolume,ArrayList<Double> _ambientTempList, double _thermalExpansionCoefficient ){
         startFluidTemp = _startFluidTemp;
         startFluidVolume = _startFluidVolume;
         ambientTempList = _ambientTempList;
+        thermalExpansionCoefficient = _thermalExpansionCoefficient;
     }
 
-    private void Run(){
+    private void run(){
         int iteration = 0;
         newFluidVolume = startFluidVolume;
         newFluidTemp = startFluidTemp;
         newAmbientTemp = ambientTempList.get(iteration);
 
-        // Algorytm...
+        while(true){
+            iteration++;
+            outputFluidTemps.add(newFluidTemp);
+            outputFluidTemps.add(newFluidVolume);
 
+            oldAmbientTemp = newAmbientTemp;
+            newAmbientTemp = ambientTempList.get(iteration);
+
+            oldFluidTemp = newFluidTemp;
+            newFluidTemp = calculateFluidTemp(newAmbientTemp, newFluidTemp);
+
+            newFluidVolume = calculateFluidVolume(newFluidVolume, newFluidTemp, oldFluidTemp, thermalExpansionCoefficient);
+        }
+
+    }
+
+    private double calculateFluidTemp(double _ambientTemp, double _currentFluidTemp){
+        // TO DO...
+        return 0;
+    }
+
+    private double calculateFluidVolume(double _fluidVolume, double _newFluidTemp, double _oldFluidTemp, double _coefficient ){
+        return _fluidVolume * (1 + _coefficient * (_newFluidTemp - _oldFluidTemp));
     }
 }
